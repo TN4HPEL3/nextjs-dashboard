49f5cc7e87aeb42e5e549ff82a11df1248a8812b
Krystian Klimczak <ctnahpele@icloud.com>
2025-02-01 00:27:10 +0100

    Wraped invoices detele button in from with delete invoice action (using JS bind function).

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 export function CreateInvoice() {
@@ -21,12 +22,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button className='rounded-md border p-2 hover:bg-gray-100'>
         <span className='sr-only'>Delete</span>
         <TrashIcon className='w-5' />
       </button>
-    </>
+    </form>
   );
 }

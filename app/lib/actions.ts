5199f8eff47e707f5d6ae6c4726d828b7760f1c6
Krystian Klimczak <ctnahpele@icloud.com>
2025-01-31 23:43:21 +0100

    Revalidate and redirect after adding new invoice.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,10 @@
 'use server';
 
 import { z } from 'zod';
+
 import postgres from 'postgres';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -30,4 +33,7 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
